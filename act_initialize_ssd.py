c9fe8d2644aa8f61232742ae67b44de8115641f6
Min Zhuo <mzhuo@google.com>
2018-08-08 11:08:17 -0700

    Add files via upload

--- act_initialize_ssd.py
+++ act_initialize_ssd.py
@@ -1,107 +1,84 @@
 #!/usr/bin/python
 ####
-# Act config creator to help configuration for tests
 ####
-
+# Act prepare SSD to run act benchmark aerospike latency verification tool.
+####
 import cmd
 import os
 import sys
 import argparse
 import subprocess
 from subprocess import call
-import re
-import time
 
 
-#act_cfg_gen(args.no_ssd, args.actwriteload, args.actreadload,  no_queue, no_thread_per_queue, args.runtime, args.actfile)
 NVME='nvme0n'
 FACTOR=1000
 
-def get_device_name(no_of_devices=0):
+def get_device_name():
     cmd = "lsblk"
     device_list = []
-    print "cmd:", cmd
+    nvmelist = []
     try:
-        templist = subprocess.check_output(cmd.split(";")).splitlines()
+        templist = subprocess.check_output(cmd.split()).splitlines()
     except Exception, i:
         print '\nException: ', i
         return  NULL
-    nvmelist = []
-    print templist
     for _temp in templist:
         print _temp
         if NVME in _temp:
-           nvmelist.append(_temp)
-    print nvmelist
+           nvmelist.append(_temp.split()[0])
     if not nvmelist:
        return NULL
-    if no_of_devices == 0 :
-       no_of_devices = len(nvmelist)
-    if no_of_devices > len(nvmelist) or no_of_devices < 1:
-       print "No enough nvme devices."
-       return NULL
-    else:
-      for device in range(no_of_devices):
-          device_list.append( '/dev/' + nvmelist[device].split()[0])
-    print device_list
+    for device in nvmelist:
+        device_list.append( '/dev/{}'.format(device))
     return device_list
-
-def kill_all_actprep():
-    cmd = "sudo killall actprep"
-    print "cmd:", cmd
-    try:
-        subprocess.check_output(cmd.split()).splitlines()
-        return True
-    except Exception, i:
-        print '\nException: ', i
-        return False
-
-def stop_all_act_process():
-    cmd = "sudo killall act"
-    print "cmd:", cmd
-    try:
-        subprocess.check_output(cmd.split()).splitlines()
-        return True
-    except Exception, i:
-        print '\nException: ', i
-        return False
 
 
 def act_initialize_ssd(device_list):
-#    cmd = "cd act"
-#    print "cmd:", cmd
-#    try:
-#        subprocess.call(cmd.split())
-#        return True
-#    except Exception, i:
-#        print '\nException: ', i
-#        return False
-    print "ssd device list:", device_list
     for _ssd in device_list:
         cmd = "sudo ./actprep {} ".format(_ssd)
-        print "cmd:", cmd
-        try: 
+        try:
             subprocess.Popen(cmd.split())
             return True
         except Exception, i:
             print '\nException: ', i
             return False
 
+def is_initialize_running():
+    cmd = "ps -ef"
+    try:
+        templine = subprocess.check_output(cmd.split()).splitlines()
+        for _temp in templine:
+            if './actprep' in _temp.split():
+                return True
+        return False
+    except Exception, i:
+        print '\nException: ', i
+        return False
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-device_type', '--device_type', dest='device_type', type=str, default='nvme')
 parser.add_argument('-device_list', '--device_list', dest='device_list', type=str)
+parser.add_argument('-check_init_status', '--check_init_status', dest='check_init_status', type=bool, default=True)
+parser.add_argument('-start_ssd_init', '--start_ssd_init', dest='start_ssd_init', type=bool, default=False)
+
+
 
 args = parser.parse_args()
 
-if args.device_type == "nvme":
-    ssd_device_list = get_device_name(0)
-else:
-    ssd_device_list = args.device_list.split()
+if args.start_ssd_init:
+    if args.device_type == "nvme":
+        ssd_device_list = get_device_name()
+    else:
+        ssd_device_list = args.device_list.split()
 
-kill_all_actprep()
-stop_all_act_process()
+    if len(ssd_device_list) > 0:
+        if not act_initialize_ssd(ssd_device_list):
+            print "Fails to initialize device."
 
-if not act_initialize_ssd(ssd_device_list):
-    print "Fails to initialize device."
+if args.check_init_status:
+    if is_initialize_running():
+        print "SSD init is running."
+    else:
+        print "SSD init is not running."
