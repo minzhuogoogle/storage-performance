d1dcec50f7e8c3963f00cd812e281dc1d55c5807
Min Zhuo <mzhuo@google.com>
2018-08-20 16:00:18 -0700

    Add files via upload

--- fio.test.sh
+++ fio.test.sh
@@ -7,85 +7,29 @@
 # Run one cli to upgrade image on multiple CfMs defined in cfmfile.
 # If # is added in front of mgmt_ip in cfmfile, this cfm will be skipped.
 
-if [ $# -ne 2 ]; then
-echo "Usage: fiobatch.sh fiobatchtest.log fiobatch.cfg"
-echo "cfgfile defines the list of CfM with mgmt ipv4, and platform"
-echo "Example:  ./upgradecfm.sh  hosts.bst.txt  R64-10176.0.0"
-echo "Output of cfgfile:
-100.123.170.8 guado
-100.123.174.2 guado
-#100.107.146.4 guado # this cfm will be skipped
-100.107.146.22 guado
-100.107.146.16 guado
-100.107.146.3 guado
-100.107.146.21 guado
-100.107.146.27 guado
-"
-exit $ERR
-fi
-
+#if [ $# -ne 2 ]; then
+#echo "Usage: fiobatch.sh fiobatchtest.log fiobatch.cfg"
+#echo "cfgfile defines the list of CfM with mgmt ipv4, and platform"
+#echo "Example:  ./upgradecfm.sh  hosts.bst.txt  R64-10176.0.0"
+#"
+#exit $ERR
+#fi
 
 
 for pattern in read write rw randread randwrite randrw
 do for iodepth in 128 256 512
    do for blocksize in  16k 32k 64k
-      do for filesize in 512k 
+      do for filesize in 512M 1024M
          do for numjobs in 4 8
             do NOW=$(date +"%m.%d.%Y")
             HOSTNAME=$(hostname)
-            rbwvalue=0
-            riopsvalue=0
-            rlatvalue=0
-            wbwvalue=0
-            wiopsvalue=0
-            wlatvalue=0
-
             fio --name=$pattern.data --iodepth=$iodepth --rw=$pattern --bs=$blocksize  \
-                        --direct=1 --size=$filesize --numjobs=$numjobs  --fsync=1 \
-                        --group_reporting --time_based --runtime=60 \
+                        --direct=1 --size=$filesize --numjobs=$numjobs  --fsync=1 --do_verify=1 --verify_fatal=1 \
+                        --rwmixread=70 --rwmixwrite=30 --group_reporting --time_based --runtime=60 \
                         --output=fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log
-            echo "=================================="
-            infoline=$(printf "pattern=%s iodepth=%s blocksize=%s filesize=%s numjobs=%s time=%s Hostname=%s" "$pattern $iodepth $blocksize $filsize $numjobs $NOW $HOSTNAME")
-            echo $infoline
-            templine=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log)
-            echo $templine
-            echo "================="
-            readorwrite=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log |  awk -v N=1 '{print $N}')
-            echo $readorwrite
-            if [ "$readorwrite" = "read" ]; then 
-               echo "this is only read"
-               rbwvalue=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | awk -v N=4 '{print $N}')
-               riopsvalue=$(grep  "runt" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log |  awk -v N=5 '{print $N}')
-               rlatvalue=$(grep "clat" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep -v percentiles | awk -v N=5 '{print $N}')
-            elif [ "$readorwrite" = "write:" ]; then 
-               echo "this is only write"
-               wbwvalue=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | awk -v N=3 '{print $N}')
-               wiopsvalue=$(grep  "runt" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log |  awk -v N=4 '{print $N}')
-               wlatvalue=$(grep "clat" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep -v percentiles | awk -v N=5 '{print $N}')
-            else   
-               echo "this is write/read"
-               rbwvalue=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep read | awk -v N=4 '{print $N}')
-               wbwvalue=$(grep 'runt' fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep write | awk -v N=3 '{print $N}')
-               riopsvalue=$(grep  "runt" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep read |  awk -v N=5 '{print $N}')
-               wiopsvalue=$(grep  "runt" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log | grep write | awk -v N=4 '{print $N}')
-               rlatvalue=$(grep "read : io" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log -A 4 | grep clat | grep -v percentiles | awk -v N=5 '{print $N}')
-               wlatvalue=$(grep "write: io" fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log -A 4 | grep clat | grep -v percentiles | awk -v N=5 '{print $N}')
-            fi
-            echo "bw:" $rbwvalue, $wbwvalue
-            echo "iops:" $riopsvalue, $wiopsvalue
-            echo "latency:" $rlatvalue, $wlatvalue
-            rbwvalue=${rbwvalue:3}
-            riopsvalue=${riopsvalue:5}
-            rlatvalue=${rlatvalue:4}
-            wbwvalue=${wbwvalue:3}
-            wiopsvalue=${wiopsvalue:5}
-            wlatvalue=${wlatvalue:4}
-            echo pattern $iodepth $blocksize $filsize $numjobs $NOW $HOSTNAME >> fio.log
-            echo $rbwvalue $riopsvalue $rlatvalue $wbwvalue $wiopsvalue $wlatvalue >> fio.log
-            
-            rm -rf *.data
+            rm -rf $pattern.data.*
             done
          done
-       done   
+       done
    done
 done
