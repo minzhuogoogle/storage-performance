6725885cb1a3e4b42c4ce99b3f2213f491b1e80d
Min Zhuo <mzhuo@google.com>
2018-08-20 23:33:47 -0700

    Update fio.test.sh

--- fio.test.sh
+++ fio.test.sh
@@ -20,12 +20,12 @@ for pattern in read write rw randread randwrite randrw
 do for iodepth in 256
    do for blocksize in  32k
       do for filesize in 1024M
-         do for numjobs in 32
+         do for numjobs in 16
             do NOW=$(date +"%m.%d.%Y")
             HOSTNAME=$(hostname)
             fio --name=$pattern.data --iodepth=$iodepth --rw=$pattern --bs=$blocksize  \
                         --direct=1 --size=$filesize --numjobs=$numjobs  --fsync=1 --do_verify=1 --verify_fatal=1 \
-                        --rwmixread=70 --rwmixwrite=30 --group_reporting --time_based --runtime=60 \
+                        --rwmixread=70 --rwmixwrite=30 --group_reporting --time_based --runtime=600 \
                         --output=fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log
             rm -rf $pattern.data.*
             done
