3b88b43a6d98605bc3e0b5d8877fb913df4331d2
Min Zhuo <mzhuo@google.com>
2018-08-22 16:23:12 -0700

    Add files via upload

--- quickfio.test.sh
+++ quickfio.test.sh
@@ -6,17 +6,17 @@
 #
 
 
-for pattern in read write rw randread randwrite randrw
-do for iodepth in 64 128 256 512
-   do for blocksize in  4k 8k 16k 32k 64k
-      do for filesize in 512M 1024 2048M
-         do for numjobs in 4 8 16 32
+for pattern in read randrw
+do for iodepth in  128
+   do for blocksize in  16k
+      do for filesize in 512M
+         do for numjobs in  16
             do NOW=$(date +"%m.%d.%Y")
             HOSTNAME=$(hostname)
             fio --name=$pattern.data --iodepth=$iodepth --rw=$pattern --bs=$blocksize  \
                         --direct=1 --size=$filesize --numjobs=$numjobs  --fsync=1 --do_verify=1 --verify_fatal=1 \
-                        --rwmixread=70 --rwmixwrite=30 --group_reporting --time_based --runtime=600 \
-                        --output=fio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log
+                        --rwmixread=70 --rwmixwrite=30 --group_reporting  \
+                        --output=qfio.$pattern.$iodepth.$blocksize.$filesize.$numjobs.$NOW.$HOSTNAME.log
             rm -rf $pattern.data.*
             done
          done
