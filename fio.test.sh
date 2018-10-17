6eae01fb4932a1a0e1872384851f3a94c9617c7d
Min Zhuo <mzhuo@google.com>
2018-10-17 14:21:35 -0700

    Update fio.test.sh

--- fio.test.sh
+++ fio.test.sh
@@ -7,7 +7,7 @@
 
 
 for pattern in read write rw randread randwrite randrw
-do for iodepth in 64 128 256 512
+do for iodepth in 64
    do for blocksize in  4k 8k 16k 32k 64k
       do for filesize in 512M 1024 2048M
          do for numjobs in 4 8 
